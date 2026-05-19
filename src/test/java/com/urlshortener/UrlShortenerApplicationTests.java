ff1b6579367f1c6aaabf77bc1b835f5f53a1ca89
taegyun <needle12s@gmail.com>
2026-05-19 23:01:24 +0900

    feat: 애플리케이션 부트스트랩 + 멀티 프로필 설정 + Context Load 테스트
    - UrlShortenerApplication: Spring Boot 엔트리포인트
    - application.yml: local(MySQL) / test(H2) 프로필 분리
      - 환경변수 기반 DB 연결 (${VAR:default} 패턴)
      - Actuator + Prometheus 메트릭 노출
      - HikariCP 풀 설정
    - UrlShortenerApplicationTests: Context Load 안전망 (test 프로필 활성화)

---
+++ src/test/java/com/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.urlshortener;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class UrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
